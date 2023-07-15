1663f203f647941eb675b2ee35c4811db197a93a
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-07-15 23:42:58 +0200

    install eleventy input output

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = (eleventyConfig) =>{
+    eleventyConfig.addPassthroughCopy({
+        "./input/assets/styles/dist/": "styles/",
+        "./input/assets/images": "images/",
+        "./input/assets/fonts": "fonts/",
+    });
+
+      return{
+        dir: {
+            input: "input/pages",
+            output: "output",
+            includes: "../_partials",
+            layouts: "../_layouts",
+            data: "../_data"
+        }
+    }
+}
